e2d97e4e8136fa0f17074b8cbbd611a03457c29d
Deni Shakh <admin@MacBook-dl.local>
2017-04-06 16:23:49 -0400

    adding favicon

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ class Project(models.Model):
     name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
     title = models.CharField(max_length=200)
-    description = models.CharField(max_length=500)
+    description = models.TextField(max_length=500)
 
     def publish(self):
         self.save()
