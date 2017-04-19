5a4eccc4da8b852eee2493df70660084795ff27d
Deni Shakh <admin@MacBook-dl.local>
2017-04-19 16:35:43 -0400

    Revert "commit"
    This reverts commit b59845fca132ba98f802f03334ad35826d2566f9.

--- blog/urls.py
+++ blog/urls.py
@@ -6,12 +6,9 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^project', views.project, name='project'),
-<<<<<<< HEAD
     url(r'^completed', views.completed, name='completed'),
     url(r'^post_list$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^admin/', include(admin.site.urls)),
 
-=======
->>>>>>> parent of df76a63... commit
 ]
