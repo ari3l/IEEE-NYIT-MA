b59845fca132ba98f802f03334ad35826d2566f9
Deni Shakh <admin@MacBook-dl.local>
2017-04-19 16:35:35 -0400

    commit

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from .models import Event, Project, Post
+from .models import Event, Project
 
 # Register your models here.
 admin.site.register(Event)
 admin.site.register(Project)
-admin.site.register(Post)
-
--- blog/views.py
+++ blog/views.py
@@ -2,15 +2,14 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .forms import ProjectForm
 
-from .models import Event, Post
+from .models import Event
 
 # Create your views here.
 
 
 def index(request):
     events = Event.objects.filter().order_by('start')
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')[:2]
-    return render(request, 'blog/index.html', {'events': events,'posts': posts})
+    return render(request, 'blog/index.html', {'events': events})
 
 def project(request):
     if request.method == "POST":
@@ -24,15 +23,3 @@ def project(request):
     else:
         form = ProjectForm()
     return render(request, 'blog/project.html', {'form': form})
-
-def completed(request):
-    return render(request, 'blog/completed.html')
-
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
-
-
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
